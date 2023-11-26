da21c680a6dc2ec8ce3abff0902cf5eb214282e5
Rouzbeh Rahbar <rouzbeh.rahbar4@gmail.com>
2023-11-26 04:16:00 +0100

    Hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Header from "@/components/Layout/Header";
+import Hero from "@/components/Layout/Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
