8632ddf378fae8ea0eadd56d681a3ff8d93003de
Rouzbeh Rahbar <rouzbeh.rahbar4@gmail.com>
2023-11-28 07:18:29 +0100

    Register component

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,54 @@
-import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
 import HomeMenu from "@/components/layout/HomeMenu";
+import SectionHeaders from "@/components/layout/SectionHeaders";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
       <HomeMenu />
+
+      {/* ******************************** ABOUT SECTION ******************** */}
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
+        <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries.
+          </p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries.
+          </p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+        </div>
+      </section>
+
+      {/* ******************************** CONTACT SECTION ******************** */}
+      <section className="text-center my-8">
+        <SectionHeaders
+          subHeader={"Don't hesitate"}
+          mainHeader={"Contact us"}
+        />
+        <div className="mt-8">
+          <a
+            className="text-4xl underline text-gray-500"
+            href="tel:+436767772749"
+          >
+            +43 676 7772749
+          </a>
+        </div>
+      </section>
     </>
   );
 }
